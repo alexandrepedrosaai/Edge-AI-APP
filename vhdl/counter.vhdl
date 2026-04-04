c1fad04be456a9e480ae867b2d74d9a02c49a55d
Alexandre Pedrosa <232003965+alexandrepedrosaai@users.noreply.github.com>
2026-04-03 23:46:30 -0300

    Add simple 4-bit counter in VHDL

---
+++ vhdl/counter.vhdl
@@ -0,0 +1,26 @@
+-- Simple 4-bit counter in VHDL
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity counter is
+    Port (
+        clk   : in  STD_LOGIC;
+        reset : in  STD_LOGIC;
+        count : out STD_LOGIC_VECTOR (3 downto 0)
+    );
+end counter;
+
+architecture Behavioral of counter is
+    signal cnt : STD_LOGIC_VECTOR (3 downto 0) := "0000";
+begin
+    process(clk, reset)
+    begin
+        if reset = '1' then
+            cnt <= "0000";
+        elsif rising_edge(clk) then
+            cnt <= cnt + 1;
+        end if;
+    end process;
+    count <= cnt;
+end Behavioral;
