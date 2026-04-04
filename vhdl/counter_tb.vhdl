89ea39beb1176470a1ba76efaa2b1a942b7c4be2
Alexandre Pedrosa <232003965+alexandrepedrosaai@users.noreply.github.com>
2026-04-03 23:47:12 -0300

    Create counter_tb.vhdl

---
+++ vhdl/counter_tb.vhdl
@@ -0,0 +1,44 @@
+-- Testbench for counter in VHDL
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity counter_tb is
+end counter_tb;
+
+architecture Behavioral of counter_tb is
+    signal clk   : STD_LOGIC := '0';
+    signal reset : STD_LOGIC := '1';
+    signal count : STD_LOGIC_VECTOR (3 downto 0);
+
+    component counter
+        Port (
+            clk   : in  STD_LOGIC;
+            reset : in  STD_LOGIC;
+            count : out STD_LOGIC_VECTOR (3 downto 0)
+        );
+    end component;
+
+begin
+    uut: counter port map (
+        clk => clk,
+        reset => reset,
+        count => count
+    );
+
+    clk_process : process
+    begin
+        while true loop
+            clk <= not clk;
+            wait for 5 ns;
+        end loop;
+    end process;
+
+    stim_proc: process
+    begin
+        wait for 10 ns;
+        reset <= '0';
+        wait for 100 ns;
+        assert false report "Simulation finished" severity failure;
+    end process;
+end Behavioral;
