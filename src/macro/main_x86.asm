acc55659f441e8c06079b004db8e80d4690e7d2a
Alexandre Pedrosa <232003965+alexandrepedrosaai@users.noreply.github.com>
2026-04-07 01:47:49 -0300

    Co-authored-by: Microsoft <opensource@microsoft.com>
    Co-authored-by: Microsoft <opensource@microsoft.com>

---
+++ src/macro/main_x86.asm
@@ -0,0 +1,22 @@
+; src/macro/main_x86.asm
+%macro LOAD 2
+    mov %1, %2
+%endmacro
+
+%macro ADD 2
+    add %1, %2
+%endmacro
+
+section .data
+    val db 5
+
+section .text
+    global _start
+
+_start:
+    LOAD rbx, [val]
+    ADD rbx, 3
+
+    mov rax, 60   ; syscall: exit
+    xor rdi, rdi
+    syscall
