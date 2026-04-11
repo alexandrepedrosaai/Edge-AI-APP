f09903f2e178d96baeba021b675fbe4181f697ca
Alexandre Pedrosa <232003965+alexandrepedrosaai@users.noreply.github.com>
2026-04-11 12:26:40 -0300

    sbcl --script EdgeAIManager.lisp
    sbcl --script EdgeAIManager.lisp

---
+++ src/EdgeAIManager.lisp
@@ -0,0 +1,39 @@
+;; EdgeAIManager.lisp
+;; Edge-AI-APP
+;; Created by Alexandre on 11/04/2026
+;; Example extensive Lisp file
+
+(defstruct manager
+  model-name
+  is-configured)
+
+(defun create-manager (name)
+  (make-manager :model-name (string-trim " " name)
+                :is-configured nil))
+
+(defun load-model (m)
+  (format t "Loading model: ~a~%" (manager-model-name m))
+  (sleep 1)
+  (format t "Model ~a loaded successfully.~%" (manager-model-name m)))
+
+(defun configure (m options)
+  (format t "Configuring EdgeAIManager with options:~%")
+  (dolist (opt options)
+    (format t "  ~a = ~a~%" (car opt) (cdr opt)))
+  (setf (manager-is-configured m) t)
+  m)
+
+(defun process-input (input)
+  (format t "Processing input of length: ~d~%" (length input))
+  (let ((output (coerce (reverse (coerce input 'list)) 'string)))
+    (format t "Processing complete.~%")
+    output))
+
+;; -------------------------------
+;; Usage Example
+;; -------------------------------
+(let* ((manager (create-manager " edge_ai_model.onnx "))
+       (configured (configure manager '(("quantization" . "int8")
+                                        ("device" . "CPU"))))
+       (output (process-input "HelloEdgeAI")))
+  (format t "Output result: ~a~%" output))
