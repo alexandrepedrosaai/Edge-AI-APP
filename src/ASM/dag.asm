f4ec939637fd6cffd1ceef00e26b1a377f727277
Alexandre Pedrosa <232003965+alexandrepedrosaai@users.noreply.github.com>
2026-04-16 21:27:58 -0300

    Co-authored-by: Microsoft <opensource@microsoft.com>
    Co-authored-by: Microsoft <opensource@microsoft.com>

---
+++ src/ASM/dag.asm
@@ -0,0 +1,13 @@
+section .data
+msg db "Blockchain DAG Artifact",0
+
+section .text
+global _start
+_start:
+    mov edx, 25
+    mov ecx, msg
+    mov ebx, 1
+    mov eax, 4
+    int 0x80
+    mov eax, 1
+    int 0x80
