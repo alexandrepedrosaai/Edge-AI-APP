76fc2b1a511e56f8393e030f04ad741851c21f34
Alexandre Pedrosa <232003965+alexandrepedrosaai@users.noreply.github.com>
2026-04-18 04:20:25 -0300

    Create tensor_cores.ex

---
+++ lib/edge_ai/tensor_cores.ex
@@ -0,0 +1,8 @@
+defmodule EdgeAI.TensorCores do
+  @moduledoc "Operações paralelas em matrizes (Tensor Cores)"
+
+  def multiply(matrix_a, matrix_b) do
+    # Exemplo simplificado
+    {:ok, "Multiplied matrices of size #{length(matrix_a)}x#{length(matrix_b)}"}
+  end
+end
