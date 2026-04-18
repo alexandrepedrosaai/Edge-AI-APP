fe41abe3218f344acea0339aaae4330c5832514f
Alexandre Pedrosa <232003965+alexandrepedrosaai@users.noreply.github.com>
2026-04-18 04:04:57 -0300

    Create application.ex

---
+++ lib/edge_ai/application.ex
@@ -0,0 +1,28 @@
+defmodule EdgeAI.Application do
+  @moduledoc """
+  Supervision tree para o Edge-AI-APP.
+  Cada módulo (ISA, GNN, Quantum, Petri, Tensor, Category, Blockchain)
+  é supervisionado como processo OTP, garantindo concorrência e resiliência.
+  """
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Supervisores de cluster (se usar libcluster)
+      {Cluster.Supervisor, [Application.get_env(:libcluster, :topologies), [name: EdgeAI.ClusterSupervisor]]},
+
+      # Módulos principais
+      {EdgeAI.ISAMatrix, []},
+      {EdgeAI.GNN, []},
+      {EdgeAI.QuantumCircuits, []},
+      {EdgeAI.PetriNets, []},
+      {EdgeAI.TensorCores, []},
+      {EdgeAI.CategoryTheory, []},
+      {EdgeAI.BlockchainDAGs, []}
+    ]
+
+    opts = [strategy: :one_for_one, name: EdgeAI.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
